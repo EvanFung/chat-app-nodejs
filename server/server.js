d59d8d2e901094058bb8bb635b1524f484aa4d3f
EvanFung <evanfungv@gmail.com>
2017-02-24 08:22:31 +0000

    the initial commit of chat app

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const express = require('express');
+const publicPath = path.join(__dirname,'../public');
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(process.env.PORT,process.env.IP,function() {
+    console.log('server is up on port '+process.env.PORT);    
+});
